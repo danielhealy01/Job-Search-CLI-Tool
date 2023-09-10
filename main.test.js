e31a4180e707408716f5c266793117cbc7b8d546
DanielHealy <danielhealyhosting3@gmail.com>
2023-09-10 10:22:59 +0100

    repeat random number test 20 times

--- main.test.js
+++ main.test.js
@@ -1,11 +1,10 @@
-import {
-	randomInterval,
-	minTime,
-	maxTime,
-} from './utils/utilityFunctions.js';
-
-
+import { randomInterval, minTime, maxTime } from './utils/utilityFunctions.js';
 
 test('random time returned should be within min and max', () => {
-	expect(randomInterval(maxTime, minTime)).toBeGreaterThan(minTime);
+	const numberOfTests = 20;
+	for (let i = 0; i < 20; i++) {
+		console.log(randomInterval(maxTime, minTime));
+		expect(randomInterval(maxTime, minTime)).toBeGreaterThan(minTime);
+		expect(randomInterval(maxTime, minTime)).toBeLessThan(maxTime);
+	}
 });
